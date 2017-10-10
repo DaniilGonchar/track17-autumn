1ee4d3f3467d60e2d1354a404654662324e864c8
DanielGonchar <dannygonchar@gmail.com>
2017-10-10 18:56:40 +0300

    removed stringbuilder and fixed input

--- L2-objects/src/main/java/ru/track/io/TaskImplementation.java
+++ L2-objects/src/main/java/ru/track/io/TaskImplementation.java
@@ -7,6 +7,8 @@ import ru.track.io.vendor.FileEncoder;
 import ru.track.io.vendor.ReferenceTaskImplementation;
 
 import java.io.File;
+import java.io.*;
+
 import java.io.IOException;
 
 public final class TaskImplementation implements FileEncoder {
@@ -20,7 +22,43 @@ public final class TaskImplementation implements FileEncoder {
     @NotNull
     public File encodeFile(@NotNull String finPath, @Nullable String foutPath) throws IOException {
         /* XXX: https://docs.oracle.com/javase/8/docs/api/java/io/File.html#deleteOnExit-- */
-        throw new UnsupportedOperationException(); // TODO: implement
+
+
+
+        File fin = new File(finPath);
+        File fout;
+
+        if (foutPath == null) {
+            fout = File.createTempFile("Base64OutputFile", ".txt");
+            fout.deleteOnExit();
+        } else {
+            fout = new File(foutPath);
+        }
+
+
+        try (InputStream is = new FileInputStream(fin); FileWriter fw = new FileWriter(fout)) {
+            while (true) {
+                int c0 = is.read();
+                if (c0 == -1)
+                    break;
+                int c1 = is.read();
+                int c2 = is.read();
+
+                int block = ((c0 & 0xFF) << 16) | ((Math.max(c1, 0) & 0xFF) << 8) | (Math.max(c2, 0) & 0xFF);
+
+                fw.write(toBase64[block >> 18 & 63]);
+                fw.write(toBase64[block >> 12 & 63]);
+                fw.write(c1 == -1 ? '=' : toBase64[block >> 6 & 63]);
+                fw.write(c2 == -1 ? '=' : toBase64[block & 63]);
+
+            }
+
+        }
+
+        return fout;
+
+
+
     }
 
     private static final char[] toBase64 = {
@@ -32,9 +70,9 @@ public final class TaskImplementation implements FileEncoder {
     };
 
     public static void main(String[] args) throws IOException {
-        final FileEncoder encoder = new ReferenceTaskImplementation();
+        final FileEncoder encoder = new TaskImplementation();
         // NOTE: open http://localhost:9000/ in your web browser
-        new Bootstrapper(args, encoder).bootstrap(9000);
+        new Bootstrapper(args, encoder).bootstrap(9003);
     }
 
 }
